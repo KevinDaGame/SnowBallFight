031927a7064244d09c246f2d3d811b5f0163b747
KevinDaGame <65958288+KevinDaGame@users.noreply.github.com>
2021-12-05 19:56:30 +0100

    Feature/creategameparamscommand (#22)
    * Added custom command option and more help
    
    * Fixed bunch of bugs
    
    * Updated pom
    
    * Refactored code

--- src/main/java/com/github/kevindagame/commands/SnowBallFightTabCompleter.java
+++ src/main/java/com/github/kevindagame/commands/SnowBallFightTabCompleter.java
@@ -17,35 +17,28 @@ public class SnowBallFightTabCompleter implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
-        List<String> completion = new ArrayList<>();
         if (args.length == 1) {
-            completion.add("give");
-            completion.add("help");
-            completion.add("start");
-            completion.add("create");
-            completion.add("join");
-            completion.add("arena");
-            completion.add("stop");
-            return completion;
+            return mainHelp();
         }
 
         if (args.length == 2) {
             switch (args[0]) {
                 case "create":
+                    List<String> completion = new ArrayList<>();
                     completion.addAll(snowBallFight.getArenaHandler().getArenas().keySet());
                     return completion;
                 case "arena":
-                    completion.add("create");
-                    completion.add("remove");
-                    completion.add("list");
-                    completion.add("info");
-                    completion.add("teams");
-                    return completion;
+                    return arenaHelp();
             }
         }
 
         if (args.length == 3) {
+            if(args[0].equals("create")){
+                completion.add("<rounds>");
+                return completion;
+            }
             if ("arena".equals(args[0])) {
+                List<String> completion = new ArrayList<>();
                 switch (args[1]) {
                     case "remove":
                         completion.addAll(snowBallFight.getArenaHandler().getArenas().keySet());
@@ -59,9 +52,14 @@ public class SnowBallFightTabCompleter implements TabCompleter {
             }
         }
         if (args.length == 4) {
+            if(args[0].equals("create")){
+                completion.add("<minutes>");
+                return completion;
+            }
             if ("arena".equals(args[0])) {
                 if ("teams".equals(args[1])) {
                     if ("add".equals(args[2])) {
+                        List<String> completion = new ArrayList<>();
                         completion.addAll(snowBallFight.getArenaHandler().getArenas().keySet());
                         return completion;
                     }
@@ -70,24 +68,55 @@ public class SnowBallFightTabCompleter implements TabCompleter {
         }
 
         if (args.length == 5) {
+            if(args[0].equals("create")){
+                completion.add("<teamplayers>");
+                return completion;
+            }
             if ("arena".equals(args[0])) {
                 if ("teams".equals(args[1])) {
                     if ("add".equals(args[2])) {
-                        completion.add("WHITE");
-                        completion.add("BLACK");
-                        completion.add("RED");
-                        completion.add("AQUA");
-                        completion.add("BLUE");
-                        completion.add("GOLD");
-                        completion.add("GRAY");
-                        completion.add("GREEN");
-                        completion.add("YELLOW");
-
-                        return completion;
+                        return getColors();
                     }
                 }
             }
         }
         return null;
+    }
+
+    private List<String> getColors() {
+        List<String> completion = new ArrayList<>();
+        completion.add("WHITE");
+        completion.add("BLACK");
+        completion.add("RED");
+        completion.add("AQUA");
+        completion.add("BLUE");
+        completion.add("GOLD");
+        completion.add("GRAY");
+        completion.add("GREEN");
+        completion.add("YELLOW");
+
+        return completion;
+    }
+
+    private List<String> arenaHelp() {
+        List<String> completion = new ArrayList<>();
+        completion.add("create");
+        completion.add("remove");
+        completion.add("list");
+        completion.add("info");
+        completion.add("teams");
+        return completion;
+    }
+
+    private List<String> mainHelp() {
+        List<String> completion = new ArrayList<String>();
+        completion.add("give");
+        completion.add("help");
+        completion.add("start");
+        completion.add("create");
+        completion.add("join");
+        completion.add("arena");
+        completion.add("stop");
+        return completion;
     }
 }
