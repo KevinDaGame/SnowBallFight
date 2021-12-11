6e55a038454191396f5151e1741371a8124f9284
KevinDaGame <65958288+KevinDaGame@users.noreply.github.com>
2021-12-11 11:40:35 +0100

    Handled some possible errors with the commands (#26)

--- src/main/java/com/github/kevindagame/Model/Team.java
+++ src/main/java/com/github/kevindagame/Model/Team.java
@@ -1,12 +1,14 @@
 package com.github.kevindagame.Model;
 
+import org.bukkit.Location;
+
 public class Team {
     final String color;
     final SpawnPoint spawnPoint;
 
-    public Team(String color, SpawnPoint spawnPoint) {
+    public Team(String color, Location l) {
         this.color = color;
-        this.spawnPoint = spawnPoint;
+        this.spawnPoint = new SpawnPoint(l.getBlockX(), l.getBlockY(), l.getBlockZ(), l.getPitch(), l.getYaw(), l.getWorld().getName());
     }
 
     public SpawnPoint getSpawnPoint() {
